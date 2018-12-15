657ae2998d22df4212c9806e57577fabb6611818
dhruvramani <dhruvramani98@gmail.com>
2018-12-15 18:38:39 +0300

    sie

--- src/model.py
+++ src/model.py
@@ -32,6 +32,7 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
+        print(x.size())
         x = x.view(x.size()[0], x.size()[1] * x.size()[2] * x.size()[3])
         x = self.classifier(x)
         return x
