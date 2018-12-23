a68166e91f02979fea5f4f9702916577c9430e42
Prathamtangri1 <prathamtangri2015@gmail.com>
2018-12-23 14:20:34 +0530

    small change

--- src/model.py
+++ src/model.py
@@ -71,29 +71,6 @@ class CNNNet(nn.Module):
         x = self.classifier(x)
         return x
 
-class PrathamNetwork(nn.Module):
-    def __init__(self):
-        super(PrathamNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(20*29*29, 4096)
-        self.fc2 = nn.Linear(4096, 5)
-
-        # self.enc.load_state_dict(torch.load('../save/checkpoint/.ckpt'))
-
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        # print(x.shape)
-        x = x.view(x.size()[0], 20 * 29 * 29)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
-
 class exp1(nn.Module):
     def __init__(self):
         super(exp1,self).__init__()
