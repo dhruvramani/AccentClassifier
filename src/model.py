e854d5218dc68b8a8156c30eded5db0b20bf7d79
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-12 21:23:05 +0530

    filters changed

--- src/model.py
+++ src/model.py
@@ -37,8 +37,8 @@ class AlexNet(nn.Module):
 class RowCNN(nn.Module):
     def __init__(self, num_classes=3, split_size=120):
         super(RowCNN, self).__init__()
-        self.window_sizes = [10,20,50,75]
-        self.n_filters = 64
+        self.window_sizes = [5, 10, 20]
+        self.n_filters = 32
         self.num_classes = num_classes
         self.split_size = split_size
         self.convs = nn.ModuleList([
